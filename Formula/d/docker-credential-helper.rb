754bb73bb355fef22620a57adce194f2d9652e21
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-20 14:33:07 +0000

    docker-credential-helper 0.9.6

--- Formula/d/docker-credential-helper.rb
+++ Formula/d/docker-credential-helper.rb
@@ -1,8 +1,8 @@
 class DockerCredentialHelper < Formula
   desc "Platform keystore credential helper for Docker"
   homepage "https://github.com/docker/docker-credential-helpers"
-  url "https://github.com/docker/docker-credential-helpers/archive/refs/tags/v0.9.5.tar.gz"
-  sha256 "7f2e5ed5e9c9c483288a227692b70abe283bdf78b0abc36d0846704bb6d82c94"
+  url "https://github.com/docker/docker-credential-helpers/archive/refs/tags/v0.9.6.tar.gz"
+  sha256 "aa0409f5bc2afcb9b03c567ea3d61b1009f3075cc2e80da938bff4a9c86dda54"
   license "MIT"
   head "https://github.com/docker/docker-credential-helpers.git", branch: "master"
 
